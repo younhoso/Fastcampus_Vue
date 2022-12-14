9692def84781c135be44e8f720c424b507a227be
younhoso <thdbsgh3443@kakao.com>
2022-12-14 16:55:10 +0900

    Add: Vue create template

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Fastcampus_Vue
+이 프로젝트는 vue.js를 학습하기 위한 프로젝트 입니다.
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    components: {},
+    data() {
+        return{
+            message: 'hello',
+            active: false
+        };
+    },
+    setup() {},
+    created() {},
+    mounted() {},
+    unmounted() {},
+    methods: {
+    }
+}
+</script>
+
+<template>
+  <div class="msg" v-bind:class="{'active': active}">
+    {{message}}
+  </div>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.msg.active { color: red;}
+</style>
